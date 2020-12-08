2b0072a837a5464ba97bce226c6ea81dafbb3c4e
Nathaniel Caldwell <nbcwell@gmail.com>
2020-12-08 16:53:37 -0500

    Import koinos-types and libp2p

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/koinos/koinos-p2p
 
 go 1.15
+
+require (
+	github.com/koinos/koinos-types-golang v0.1.0
+	github.com/libp2p/go-libp2p v0.12.0 // indirect
+)
--- internal/p2p/p2p_test.go
+++ internal/p2p/p2p_test.go
@@ -2,11 +2,19 @@ package p2p
 
 import (
     "testing"
+    types "github.com/koinos/koinos-types-golang"
 )
 
 func TestGetInfo(t *testing.T) {
     s := GetInfo()
     if s != "test" {
         t.Error("String does not match")
+    }
+}
+
+func TestGetNumber(t *testing.T) {
+    s := GetNumber()
+    if s != types.UInt64(10) {
+        t.Error("Number does not match")
     }
 }
