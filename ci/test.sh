03d6d8d82d94ca4bab56158ae43617021e3803e5
Nathaniel Caldwell <nbcwell@gmail.com>
2021-02-09 21:27:17 -0500

    Add inventory test to ci

--- ci/test.sh
+++ ci/test.sh
@@ -5,10 +5,13 @@ set -x
 
 go test -v github.com/koinos/koinos-p2p/internal -coverprofile=./build/internal.out -coverpkg=./internal/...
 go test -v github.com/koinos/koinos-p2p/internal/node -coverprofile=./build/node.out -coverpkg=./internal/...
+go test -v github.com/koinos/koinos-p2p/internal/inventory -coverprofile=./build/inventory.out -coverpkg=./internal/...
 
 gcov2lcov -infile=./build/internal.out -outfile=./build/internal.info
 gcov2lcov -infile=./build/node.out -outfile=./build/node.info
+gcov2lcov -infile=./build/inventory.out -outfile=./build/inventory.info
 
 lcov -a ./build/internal.info -a ./build/node.info -o ./build/merged.info
+lcov -a ./build/merged.info -a ./build/inventory.info -o ./build/merged.info
 
 golint -set_exit_status ./...
