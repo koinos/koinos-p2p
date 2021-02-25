f02b197b582e933500536828a5833a2adb5713f4
theoreticalbts <theoreticalbts@users.noreply.github.com>
2021-02-25 22:54:02 +0000

    Implement connection notification handler

---
+++ internal/protocol/notify.go
@@ -0,0 +1,57 @@
+package protocol
+
+import (
+	"context"
+	"github.com/libp2p/go-libp2p-core/host"
+	"github.com/libp2p/go-libp2p-core/network"
+	ma "github.com/multiformats/go-multiaddr"
+)
+
+// SyncManagerPeerAdder adds the existing peers and any new peers using the network.Notifiee interface.
+type SyncManagerPeerAdder struct {
+	// Golang docs advise to prefer passing context in a function parameter rather than a struct field.
+	// However we can't follow this advice, since our function parameters are constrained to what is
+	// specified in the network.Notifiee interface, which doesn't include a context parameter.
+	ctx context.Context
+
+	host        host.Host
+	syncManager *SyncManager
+}
+
+func NewSyncManagerPeerAddr(ctx context.Context, host host.Host, syncManager *SyncManager) SyncManagerPeerAdder {
+	peerAdder := SyncManagerPeerAdder{ctx, host, syncManager}
+	peerAdder.addCurrentPeers()
+	return peerAdder
+}
+
+// OpenedStream is part of the libp2p network.Notifiee interface
+func (peerAdder *SyncManagerPeerAdder) OpenedStream(n network.Network, s network.Stream) {
+}
+
+// ClosedStream is part of the libp2p network.Notifiee interface
+func (peerAdder *SyncManagerPeerAdder) ClosedStream(n network.Network, s network.Stream) {
+}
+
+// Connected is part of the libp2p network.Notifiee interface
+func (peerAdder *SyncManagerPeerAdder) Connected(n network.Network, c network.Conn) {
+	peerAdder.syncManager.AddPeer(c.RemotePeer())
+}
+
+// Disconnected is part of the libp2p network.Notifiee interface
+func (peerAdder *SyncManagerPeerAdder) Disconnected(n network.Network, c network.Conn) {
+}
+
+// Listen is part of the libp2p network.Notifiee interface
+func (peerAdder *SyncManagerPeerAdder) Listen(n network.Network, _ ma.Multiaddr) {
+}
+
+// ListenClose is part of the libp2p network.Notifiee interface
+func (peerAdder *SyncManagerPeerAdder) ListenClose(n network.Network, _ ma.Multiaddr) {
+}
+
+// Listen is part of the libp2p network.Notifiee interface
+func (peerAdder *SyncManagerPeerAdder) addCurrentPeers() {
+	for _, peer := range peerAdder.host.Network().Peers() {
+		peerAdder.syncManager.AddPeer(peer)
+	}
+}
