58ae567c0491d2afd11f6dba24c5dd432b604a95
Michael Vandeberg <mvandeberg@users.noreply.github.com>
2021-04-14 14:23:14 -0700

    Build docker image in CI

--- ci/build.sh
+++ ci/build.sh
@@ -3,6 +3,10 @@
 set -e
 set -x
 
-go get ./...
-mkdir -p build
-go build -o build/koinos_p2p cmd/koinos-p2p/main.go
+if [[ -z $BUILD_DOCKER ]]; then
+   go get ./...
+   mkdir -p build
+   go build -o build/koinos_p2p cmd/koinos-p2p/main.go
+else
+   docker build . -t koinos-p2p
+fi
--- ci/install.sh
+++ ci/install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-sudo apt-get install -y lcov ruby
-sudo gem install coveralls-lcov
-go get -u github.com/jandelgado/gcov2lcov
-go get -u golang.org/x/lint/golint
+if [[ -z $BUILD_DOCKER ]]; then
+   sudo apt-get install -y lcov ruby
+   sudo gem install coveralls-lcov
+   go get -u github.com/jandelgado/gcov2lcov
+   go get -u golang.org/x/lint/golint
+fi
