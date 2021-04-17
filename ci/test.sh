b683b4620de576b773e5effffa4372f5d5e2eb6d
Michael Vandeberg <mvandeberg@users.noreply.github.com>
2021-04-16 18:00:45 -0700

    Try to run integration tests on p2p

--- ci/test.sh
+++ ci/test.sh
@@ -13,4 +13,16 @@ if [[ -z $BUILD_DOCKER ]]; then
    lcov -a ./build/internal.info -a ./build/node.info -o ./build/merged.info
 
    golint -set_exit_status ./...
+else
+   TAG="$TRAVIS_BRANCH"
+   if [ "$TAG" = "master" ]; then
+      TAG="latest"
+   fi
+
+   export P2P_TAG=$TAG
+
+   git clone git@github.com:koinos/koinos-integration-tests.git
+
+   cd koinos-integration_tests/tests
+   ./run.sh
 fi
