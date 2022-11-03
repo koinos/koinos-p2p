3a947571ee2e07bf9811f20d45002af8bf99fd60
Nathaniel Caldwell <nbcwell@gmail.com>
2022-11-03 17:18:37 -0400

    Bump version numbers

--- ci/build.sh
+++ ci/build.sh
@@ -6,7 +6,7 @@ set -x
 if [[ -z $BUILD_DOCKER ]]; then
    go get ./...
    mkdir -p build
-   go build -o build/koinos_p2p cmd/koinos-p2p/main.go
+   go build -ldflags="-X main.Commit=$(git rev-parse HEAD)" -o build/koinos_p2p cmd/koinos-p2p/main.go
 else
    TAG="$TRAVIS_BRANCH"
    if [ "$TAG" = "master" ]; then
